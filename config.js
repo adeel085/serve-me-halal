6b4ff6c646ba47d2b14c49083e01d961e54e1666
adeel085 <adeel.ahmed9360@gmail.com>
2021-10-13 14:46:04 +0500

    Actual Description in Restaurant Page

--- config.js
+++ config.js
@@ -7,28 +7,28 @@ config.google = {}
 config.google.APIKey = "AIzaSyCPnbUmYNmXy3aX9T71GT6BjRxFWC8z_1w"
 
 // // web address config localhost
-config.web.port = process.env.PORT || 3000;
-config.web.baseURL = 'http://localhost:' + config.web.port;
-config.web.appName = 'ServeMeHalal'
+// config.web.port = process.env.PORT || 3000;
+// config.web.baseURL = 'http://localhost:' + config.web.port;
+// config.web.appName = 'ServeMeHalal'
 
 // // Local database config
-config.db.host = 'localhost';
-config.db.user = 'root';
-config.db.password = '';
-config.db.dbName = 'serve_me_halal_db';
-config.db.port = 3306;
+// config.db.host = 'localhost';
+// config.db.user = 'root';
+// config.db.password = '';
+// config.db.dbName = 'serve_me_halal_db';
+// config.db.port = 3306;
 
 
 // web address config live
-// config.web.port = process.env.PORT || 3000;
-// config.web.baseURL = 'https://servemehalal.herokuapp.com';
-// config.web.appName = 'ServeMeHalal';
+config.web.port = process.env.PORT || 3000;
+config.web.baseURL = 'https://servemehalal.herokuapp.com';
+config.web.appName = 'ServeMeHalal';
 
 // Remote Database config
-// config.db.host = 'us-cdbr-east-04.cleardb.com';
-// config.db.user = 'b9896834e3ddba';
-// config.db.password = '6bc204fa';
-// config.db.dbName = 'heroku_c80ad99034f460e';
-// config.db.port = 3306;
+config.db.host = 'us-cdbr-east-04.cleardb.com';
+config.db.user = 'b9896834e3ddba';
+config.db.password = '6bc204fa';
+config.db.dbName = 'heroku_c80ad99034f460e';
+config.db.port = 3306;
 
 module.exports = config;
